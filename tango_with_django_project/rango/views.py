ed6a11aaa28bfe6b953fd4a213b7642512e8c8b9
Zihan Liao <2709935l@student.gla.ac.uk>
2024-02-08 13:41:27 +0000

    Champter 6 final version

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
 
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
-    # Render the response and send it back!
+    context_dict['pages'] = page_list
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
@@ -31,3 +34,15 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
